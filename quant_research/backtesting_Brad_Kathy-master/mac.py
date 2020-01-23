71af62464f019d5498288a77222f0d3899460f4a
bradsun91 <bradsun91@gmail.com>
2020-01-23 20:31:02 +0800

    20200123

---
+++ quant_research/backtesting_Brad_Kathy-master/mac.py
@@ -0,0 +1,115 @@
+# mac.py
+
+print("Executing mac.py")
+from data import HistoricCSVDataHandler
+from execution import  SimulatedExecutionHandler
+from portfolio import Portfolio
+from strategy import Strategy
+from event import SignalEvent
+import numpy as np
+
+
+class MovingAverageCrossStrategy(Strategy):
+    """
+    Carries out a basic Moving Average Crossover strategy with a
+    short/long simple weighted moving average. Default short/long
+    windows are 12/26 periods respectively.
+    """
+
+    def __init__(self, bars, events, short_window=12, long_window=26):
+        """
+        Initialises the buy and hold strategy.
+
+        Parameters:
+        bars - The DataHandler object that provides bar information
+        events - The Event Queue object.
+        short_window - The short moving average lookback.
+        long_window - The long moving average lookback.
+        """
+        self.bars = bars
+        self.symbol_list = self.bars.symbol_list
+        self.events = events
+        self.short_window = short_window
+        self.long_window = long_window
+
+        # Set to True if a symbol is in the market
+        self.bought = self._calculate_initial_bought()
+
+    def _calculate_initial_bought(self):
+        """
+        Adds keys to the bought dictionary for all symbols
+        and sets them to 'OUT'.
+        """
+        bought = {}
+        for s in self.symbol_list:
+            bought[s] = 'OUT'
+        return bought
+
+    def calculate_signals(self, event):
+        """
+        Generates a new set of signals based on the MAC
+        SMA with the short window crossing the long window
+        meaning a long entry and vice versa for a short entry.    
+
+        Parameters
+        event - A MarketEvent object. 
+        """
+        if event.type == 'MARKET':
+            for symbol in self.symbol_list:
+                bars = self.bars.get_latest_bars_values(symbol, "Close", N=self.long_window)
+
+                if bars is not None and bars != []:
+                    short_sma = np.mean(bars[-self.short_window:])
+                    long_sma = np.mean(bars[-self.long_window:])
+
+                    dt = self.bars.get_latest_bar_datetime(symbol)
+                    sig_dir = ""
+                    strength = 1.0
+                    strategy_id = 1
+
+                    if short_sma > long_sma and self.bought[symbol] == "OUT":
+                        sig_dir = 'LONG'
+                        signal = SignalEvent(strategy_id, symbol, dt, sig_dir, strength)
+                        self.events.put(signal)
+                        self.bought[symbol] = 'LONG'
+
+                    elif short_sma < long_sma and self.bought[symbol] == "LONG":
+                        sig_dir = 'EXIT'
+                        signal = SignalEvent(strategy_id, symbol, dt, sig_dir, strength)
+                        self.events.put(signal)
+                        self.bought[symbol] = 'OUT'
+
+
+if __name__ == "__main__":
+    import datetime, pandas as pd
+    import matplotlib.pyplot as plt
+    from backtest import Backtest
+
+    csv_dir = r"C:\Users\ruiya\Desktop\Moyi-tech\backtest\backtest"
+    symbol_list = ['QQQ', 'SPY', 'TSLA',  'YELP']
+    initial_capital = 100000.0
+    heartbeat = 0.0
+    start_date = datetime.datetime(2010, 4, 1, 0, 0, 0)
+    start_date_str = str(start_date)
+
+    backtest = Backtest(csv_dir, 
+                        symbol_list, 
+                        initial_capital, 
+                        heartbeat,
+                        start_date,
+                        HistoricCSVDataHandler,
+                        SimulatedExecutionHandler, 
+                        Portfolio, 
+                        MovingAverageCrossStrategy)
+    
+    backtest.simulate_trading()
+
+    df_equity = pd.read_csv("equity1.csv")
+    df_equity.drop_duplicates("datetime", inplace=True)
+    # df_equity = df_equity[df_equity['datetime'] > start_date_str]
+    df_equity.index = df_equity['datetime']
+    df_equity = df_equity[df_equity['total'].map(lambda x: str(x) != "nan")]
+    df_equity['equity_curve'].plot(figsize=(16, 7))
+    plt.show()
+    # df_equity.to_csv('df_equity1.csv')
+    print(df_equity)
