54980dadaeda2fe1a645f971d1687f3184467bf3
bradsun91 <bradsun91@gmail.com>
2020-03-21 23:57:37 +0800

    20200321

--- quant_research/moyi/fulltime/minerva/backtesting_Brad_Kathy-master/mac.py
+++ quant_research/moyi/fulltime/minerva/backtesting_Brad_Kathy-master/mac.py
@@ -80,13 +80,70 @@ class MovingAverageCrossStrategy(Strategy):
                         self.bought[symbol] = 'OUT'
 
 
+class movingAverageConvergenceDivergence(Strategy):
+    def __init__(self, bars, events, short_window=12, long_window=26):
+        self.bars = bars
+        self.symbol_list = self.bars.symbol_list
+        self.events = events
+        self.short_window = short_window
+        self.long_window = long_window
+
+        # Set to True if a symbol is in the market
+        self.bought = self._calculate_initial_bought()
+
+    def _calculate_initial_bought(self):
+        """
+        Adds keys to the bought dictionary for all symbols
+        and sets them to 'OUT'.
+        """
+        bought = {}
+        for s in self.symbol_list:
+            bought[s] = 'OUT'
+        return bought
+
+    def calculate_signals(self, event):
+        """
+        Generates a new set of signals based on the MAC
+        SMA with the short window crossing the long window
+        meaning a long entry and vice versa for a short entry.
+
+        Parameters
+        event - A MarketEvent object.
+        """
+        if event.type == 'MARKET':
+            for symbol in self.symbol_list:
+                bars = self.bars.get_latest_bars_values(symbol, "Close", N=self.long_window)
+
+                if bars is not None and bars != []:
+                    exp1 = pd.Series(bars).ewm(span=12, adjust=False).mean()
+                    exp2 = pd.Series(bars).ewm(span=26, adjust=False).mean()
+                    macd = exp1 - exp2
+                    exp3 = macd.ewm(span=9, adjust=False).mean()
+                    dt = self.bars.get_latest_bar_datetime(symbol)
+                    sig_dir = ""
+                    strength = 1.0
+                    strategy_id = 1
+
+                    if macd.iloc[-1] > exp3.iloc[-1] and self.bought[symbol] == "OUT":
+                        sig_dir = 'LONG'
+                        signal = SignalEvent(strategy_id, symbol, dt, sig_dir, strength)
+                        self.events.put(signal)
+                        self.bought[symbol] = 'LONG'
+
+                    elif macd.iloc[-1] < exp3.iloc[-1] and self.bought[symbol] == "LONG":
+                        sig_dir = 'EXIT'
+                        signal = SignalEvent(strategy_id, symbol, dt, sig_dir, strength)
+                        self.events.put(signal)
+                        self.bought[symbol] = 'OUT'
+
+
 if __name__ == "__main__":
     import datetime, pandas as pd
     import matplotlib.pyplot as plt
     from backtest import Backtest
 
     csv_dir = r"C:\Users\ruiya\Desktop\Moyi-tech\backtest\backtest"
-    symbol_list = ['QQQ', 'SPY', 'TSLA',  'YELP']
+    symbol_list = ['SPY']
     initial_capital = 100000.0
     heartbeat = 0.0
     start_date = datetime.datetime(2010, 4, 1, 0, 0, 0)
@@ -99,8 +156,9 @@ if __name__ == "__main__":
                         start_date,
                         HistoricCSVDataHandler,
                         SimulatedExecutionHandler, 
-                        Portfolio, 
-                        MovingAverageCrossStrategy)
+                        Portfolio,
+                        # MovingAverageCrossStrategy)
+                        movingAverageConvergenceDivergence)
     
     backtest.simulate_trading()
 
--- quant_research/moyi/fulltime/minerva/backtesting_Brad_Kathy-master/pyplott.py
+++ quant_research/moyi/fulltime/minerva/backtesting_Brad_Kathy-master/pyplott.py
@@ -1,34 +1,68 @@
+import datetime
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from portfolio import *
-
-obj = Portfolio()
+import pandas_datareader.data as web
 
 
 
+class Visualization(object):
+    def __init__(self, bt_csv_dir, start_date_str):
+        self.equity_curve = pd.read_csv(bt_csv_dir + "equity.csv")
+        self.equity_curve.drop_duplicates("datetime", inplace=True)
+        self.equity_curve = self.equity_curve[self.equity_curve['datetime'] > start_date_str]
+        self.equity_curve.index = self.equity_curve['datetime']
+        self.equity_curve = self.equity_curve[self.equity_curve['total'].map(lambda x: str(x) != "nan")]
+        self.equity_curve.drop(['datetime'], axis=1, inplace=True)
+        self.equity_curve.index = pd.to_datetime(self.equity_curve.index, format='%Y-%m-%d')
+        self.plot_size = [18, 6]
 
+    def plot_commissions(self):
+        self.equity_curve['commission'].plot(figsize=self.plot_size)
+        plt.show()
 
+    def plot_cash(self):
+        self.equity_curve['cash'].plot(figsize=self.plot_size)
+        plt.show()
 
+    def plot_total(self):
+        self.equity_curve['total'].plot(figsize=self.plot_size)
+        plt.show()
 
+    def plot_returns(self):
+        self.equity_curve['returns'].plot(figsize=self.plot_size)
+        plt.show()
 
+    def plot_equity_curve(self):
+        self.equity_curve['equity_curve'].plot(figsize=self.plot_size)
+        plt.show()
 
+    def plot_drawdown(self):
+        self.equity_curve['drawdown'].plot(figsize=self.plot_size)
+        plt.show()
 
+    def return_equity_df(self):
+        return self.equity_curve
 
-class Visualization(Portfolio):
+    def get_SPX(self):
+        start_dt = self.equity_curve.index.min()
+        end_dt = self.equity_curve.index.max() - datetime.timedelta(1)
+        sp500 = web.DataReader('^GSPC', 'yahoo', start_dt, end_dt)['Adj Close']
+        return sp500
 
-    def __init__ (self):
-        super().__init__()
-    #     inherit the self.equity_curve from portfolio
-
-    def tsPlot(self, column_name):
-        self.equity_curve[column_name].plot()
+    def plot_comp_SPX(self, sp500):
+        comb_df = self.equity_curve[['returns']]
+        comb_df['ret_sp500'] = sp500.pct_change()
+        cumprod_df = (comb_df.dropna() + 1).cumprod()
+        cumprod_df.plot(figsize=self.plot_size)
         plt.show()
 
 
-if __name__ == "__main__":
-    # port = Visualization.create_equity_curve_dataframe
-    Visualization.tsPlot('equity_curve')
-    # obj.tsPlot('equity_curve')
+equity_folder = "./"
+start_date = datetime.datetime(2010, 4, 1, 0, 0, 0)
+start_date_str = str(start_date)
+visualization = Visualization(equity_folder, start_date_str)
 
+visualization.plot_equity_curve()
 
+sp500 = visualization.get_SPX()
+visualization.plot_comp_SPX(sp500)
